4d163519c66a1a998556fe828ae239d79a608789
bunnyhero <bunnyhero@bunnyhero.com>
2019-02-21 17:37:50 -0500

    Reformat with Black

--- AppStoreReviews.py
+++ AppStoreReviews.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-''' Apple AppStore reviews scrapper
+""" Apple AppStore reviews scrapper
     version 2015-11-04
     Original code written by Tomek "Grych" Gryszkiewicz, grych@tg.pl
      - http://www.tg.pl
@@ -9,7 +9,7 @@
      - http://blogs.oreilly.com/iphone/2008/08/scraping-appstore-reviews.html
     AppStore codes are based on "appstore_reviews" by Jeremy Wohl
      - https://github.com/jeremywohl/iphone-scripts/blob/master/appstore_reviews
-'''
+"""
 import argparse
 import csv
 import re
@@ -19,101 +19,102 @@ import urllib2
 import xml.etree.ElementTree as ElementTree
 
 reload(sys)
-sys.setdefaultencoding('utf8')
+sys.setdefaultencoding("utf8")
 
 appStores = {
-    'Argentina':          143505,
-    'Australia':          143460,
-    'Belgium':            143446,
-    'Brazil':             143503,
-    'Canada':             143455,
-    'Chile':              143483,
-    'China':              143465,
-    'Colombia':           143501,
-    'Costa Rica':         143495,
-    'Croatia':            143494,
-    'Czech Republic':     143489,
-    'Denmark':            143458,
-    'Deutschland':        143443,
-    'El Salvador':        143506,
-    'Espana':             143454,
-    'Finland':            143447,
-    'France':             143442,
-    'Greece':             143448,
-    'Guatemala':          143504,
-    'Hong Kong':          143463,
-    'Hungary':            143482,
-    'India':              143467,
-    'Indonesia':          143476,
-    'Ireland':            143449,
-    'Israel':             143491,
-    'Italia':             143450,
-    'Korea':              143466,
-    'Kuwait':             143493,
-    'Lebanon':            143497,
-    'Luxembourg':         143451,
-    'Malaysia':           143473,
-    'Mexico':             143468,
-    'Nederland':          143452,
-    'New Zealand':        143461,
-    'Norway':             143457,
-    'Osterreich':         143445,
-    'Pakistan':           143477,
-    'Panama':             143485,
-    'Peru':               143507,
-    'Phillipines':        143474,
-    'Poland':             143478,
-    'Portugal':           143453,
-    'Qatar':              143498,
-    'Romania':            143487,
-    'Russia':             143469,
-    'Saudi Arabia':       143479,
-    'Schweiz/Suisse':     143459,
-    'Singapore':          143464,
-    'Slovakia':           143496,
-    'Slovenia':           143499,
-    'South Africa':       143472,
-    'Sri Lanka':          143486,
-    'Sweden':             143456,
-    'Taiwan':             143470,
-    'Thailand':           143475,
-    'Turkey':             143480,
-    'United Arab Emirates': 143481,
-    'United Kingdom':     143444,
-    'United States':      143441,
-    'Venezuela':          143502,
-    'Vietnam':            143471,
-    'Japan':              143462,
-    'Dominican Republic': 143508,
-    'Ecuador':            143509,
-    'Egypt':              143516,
-    'Estonia':            143518,
-    'Honduras':           143510,
-    'Jamaica':            143511,
-    'Kazakhstan':         143517,
-    'Latvia':             143519,
-    'Lithuania':          143520,
-    'Macau':              143515,
-    'Malta':              143521,
-    'Moldova':            143523,
-    'Nicaragua':          143512,
-    'Paraguay':           143513,
-    'Uruguay':            143514
+    "Argentina": 143505,
+    "Australia": 143460,
+    "Belgium": 143446,
+    "Brazil": 143503,
+    "Canada": 143455,
+    "Chile": 143483,
+    "China": 143465,
+    "Colombia": 143501,
+    "Costa Rica": 143495,
+    "Croatia": 143494,
+    "Czech Republic": 143489,
+    "Denmark": 143458,
+    "Deutschland": 143443,
+    "El Salvador": 143506,
+    "Espana": 143454,
+    "Finland": 143447,
+    "France": 143442,
+    "Greece": 143448,
+    "Guatemala": 143504,
+    "Hong Kong": 143463,
+    "Hungary": 143482,
+    "India": 143467,
+    "Indonesia": 143476,
+    "Ireland": 143449,
+    "Israel": 143491,
+    "Italia": 143450,
+    "Korea": 143466,
+    "Kuwait": 143493,
+    "Lebanon": 143497,
+    "Luxembourg": 143451,
+    "Malaysia": 143473,
+    "Mexico": 143468,
+    "Nederland": 143452,
+    "New Zealand": 143461,
+    "Norway": 143457,
+    "Osterreich": 143445,
+    "Pakistan": 143477,
+    "Panama": 143485,
+    "Peru": 143507,
+    "Phillipines": 143474,
+    "Poland": 143478,
+    "Portugal": 143453,
+    "Qatar": 143498,
+    "Romania": 143487,
+    "Russia": 143469,
+    "Saudi Arabia": 143479,
+    "Schweiz/Suisse": 143459,
+    "Singapore": 143464,
+    "Slovakia": 143496,
+    "Slovenia": 143499,
+    "South Africa": 143472,
+    "Sri Lanka": 143486,
+    "Sweden": 143456,
+    "Taiwan": 143470,
+    "Thailand": 143475,
+    "Turkey": 143480,
+    "United Arab Emirates": 143481,
+    "United Kingdom": 143444,
+    "United States": 143441,
+    "Venezuela": 143502,
+    "Vietnam": 143471,
+    "Japan": 143462,
+    "Dominican Republic": 143508,
+    "Ecuador": 143509,
+    "Egypt": 143516,
+    "Estonia": 143518,
+    "Honduras": 143510,
+    "Jamaica": 143511,
+    "Kazakhstan": 143517,
+    "Latvia": 143519,
+    "Lithuania": 143520,
+    "Macau": 143515,
+    "Malta": 143521,
+    "Moldova": 143523,
+    "Nicaragua": 143512,
+    "Paraguay": 143513,
+    "Uruguay": 143514,
 }
 
-resultsFile = open("output.csv", 'wb')
+resultsFile = open("output.csv", "wb")
 wr = csv.DictWriter(
-    resultsFile, ['topic', 'review', 'version', 'user', 'rank'],
+    resultsFile,
+    ["topic", "review", "version", "user", "rank"],
     quoting=csv.QUOTE_ALL,
-    dialect='excel'
+    dialect="excel",
 )
 wr.writeheader()
 
 
 def getReviews(appStoreId, appId, maxReviews=-1):
-    ''' returns list of reviews for given AppStore ID and application Id
+    """ returns list of reviews for given AppStore ID and application Id
         return list format: [{"topic": unicode string, "review": unicode string, "rank": int}]
-    '''
+    """
 
     reviews = []
     i = 0
@@ -131,12 +132,15 @@ def getReviews(appStoreId, appId, maxReviews=-1):
 
 def _getReviewsForPage(appStoreId, appId, pageNo):
 
-    userAgent = 'iTunes/9.2 (Macintosh; U; Mac OS X 10.6)'
+    userAgent = "iTunes/9.2 (Macintosh; U; Mac OS X 10.6)"
     front = "%d-1" % appStoreId
-    url = "http://ax.phobos.apple.com.edgesuite.net/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id=%s&pageNumber=%d&sortOrdering=4&onlyLatestVersion=false&type=Purple+Software" % (
-        appId, pageNo)
+    url = (
+        "http://ax.phobos.apple.com.edgesuite.net/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id=%s&pageNumber=%d&sortOrdering=4&onlyLatestVersion=false&type=Purple+Software"
+        % (appId, pageNo)
+    )
     req = urllib2.Request(
-        url, headers={"X-Apple-Store-Front": front, "User-Agent": userAgent})
+        url, headers={"X-Apple-Store-Front": front, "User-Agent": userAgent}
+    )
     try:
         u = urllib2.urlopen(req, timeout=30)
     except urllib2.HTTPError:
@@ -145,55 +149,62 @@ def _getReviewsForPage(appStoreId, appId, pageNo):
     root = ElementTree.parse(u).getroot()
     reviews = []
     for node in root.findall(
-            '{http://www.apple.com/itms/}View/'
-            '{http://www.apple.com/itms/}ScrollView/'
-            '{http://www.apple.com/itms/}VBoxView/'
-            '{http://www.apple.com/itms/}View/'
-            '{http://www.apple.com/itms/}MatrixView/'
-            '{http://www.apple.com/itms/}VBoxView/'
-            '{http://www.apple.com/itms/}VBoxView/'):
+        "{http://www.apple.com/itms/}View/"
+        "{http://www.apple.com/itms/}ScrollView/"
+        "{http://www.apple.com/itms/}VBoxView/"
+        "{http://www.apple.com/itms/}View/"
+        "{http://www.apple.com/itms/}MatrixView/"
+        "{http://www.apple.com/itms/}VBoxView/"
+        "{http://www.apple.com/itms/}VBoxView/"
+    ):
         review = {}
 
         review_node = node.find(
-            "{http://www.apple.com/itms/}TextView/{http://www.apple.com/itms/}SetFontStyle")
+            "{http://www.apple.com/itms/}TextView/{http://www.apple.com/itms/}SetFontStyle"
+        )
         if review_node is None:
             review["review"] = None
         else:
             review["review"] = review_node.text
 
         version_node = node.find(
-            "{http://www.apple.com/itms/}HBoxView/{http://www.apple.com/itms/}TextView/{http://www.apple.com/itms/}SetFontStyle/{http://www.apple.com/itms/}GotoURL")
+            "{http://www.apple.com/itms/}HBoxView/{http://www.apple.com/itms/}TextView/{http://www.apple.com/itms/}SetFontStyle/{http://www.apple.com/itms/}GotoURL"
+        )
         if version_node is None:
             review["version"] = None
         else:
             try:
 
                 review["version"] = re.search(
-                    "Version [^\n^\ ]+", version_node.tail).group()
+                    "Version [^\n^\ ]+", version_node.tail
+                ).group()
             except:
                 review["version"] = None
 
         user_node = node.find(
-            "{http://www.apple.com/itms/}HBoxView/{http://www.apple.com/itms/}TextView/{http://www.apple.com/itms/}SetFontStyle/{http://www.apple.com/itms/}GotoURL/{http://www.apple.com/itms/}b")
+            "{http://www.apple.com/itms/}HBoxView/{http://www.apple.com/itms/}TextView/{http://www.apple.com/itms/}SetFontStyle/{http://www.apple.com/itms/}GotoURL/{http://www.apple.com/itms/}b"
+        )
         if user_node is None:
             review["user"] = None
         else:
             review["user"] = user_node.text.strip()
 
         rank_node = node.find(
-            "{http://www.apple.com/itms/}HBoxView/{http://www.apple.com/itms/}HBoxView/{http://www.apple.com/itms/}HBoxView")
+            "{http://www.apple.com/itms/}HBoxView/{http://www.apple.com/itms/}HBoxView/{http://www.apple.com/itms/}HBoxView"
+        )
         if rank_node is None:
             review["rank"] = None
         else:
             try:
-                alt = rank_node.attrib['alt']
-                st = int(alt.strip(' stars'))
+                alt = rank_node.attrib["alt"]
+                st = int(alt.strip(" stars"))
                 review["rank"] = st
             except KeyError:
                 review["rank"] = None
 
         topic_node = node.find(
-            "{http://www.apple.com/itms/}HBoxView/{http://www.apple.com/itms/}TextView/{http://www.apple.com/itms/}SetFontStyle/{http://www.apple.com/itms/}b")
+            "{http://www.apple.com/itms/}HBoxView/{http://www.apple.com/itms/}TextView/{http://www.apple.com/itms/}SetFontStyle/{http://www.apple.com/itms/}b"
+        )
         if topic_node is None:
             review["topic"] = None
         else:
@@ -206,8 +217,8 @@ def _getReviewsForPage(appStoreId, appId, pageNo):
 
 
 def _print_reviews(reviews, country):
-    ''' returns (reviews count, sum rank)
-    '''
+    """ returns (reviews count, sum rank)
+    """
     if len(reviews) > 0:
         print "Reviews in %s:" % (country)
         print ""
@@ -219,7 +230,11 @@ def _print_reviews(reviews, country):
             print " (%s) %s" % (review["topic"], review["review"])
             print ""
             sumRank += review["rank"]
-        print "Number of reviews in %s: %d, avg rank: %.2f\n" % (country, len(reviews), 1.0 * sumRank / len(reviews))
+        print "Number of reviews in %s: %d, avg rank: %.2f\n" % (
+            country,
+            len(reviews),
+            1.0 * sumRank / len(reviews),
+        )
         return (len(reviews), sumRank)
     else:
         return (0, 0)
@@ -230,19 +245,41 @@ def _print_rawmode(reviews):
         print review["topic"], review["review"].replace("\n", "")
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='AppStoreReviewsScrapper command line.',
-                                     epilog='To get your application Id look into the AppStore link to you app, for example http://itunes.apple.com/pl/app/autobuser-warszawa/id335042980?mt=8 - app Id is the number between "id" and "?mt=0"')
-    parser.add_argument('-i', '--id', default=0, metavar='AppId',
-                        type=int, help='Application Id (see below)')
-    parser.add_argument('-c', '--country', metavar='"Name"', type=str,
-                        default='all', help='AppStore country name (use -l to see them)')
-    parser.add_argument('-l', '--list', action='store_true',
-                        default=False, help='AppStores list')
-    parser.add_argument('-m', '--max-reviews', default=-1,
-                        metavar='MaxReviews', type=int, help='Max number of reviews to load')
-    parser.add_argument('-r', '--raw-mode', action='store_true',
-                        default=False, help='Print raw mode')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="AppStoreReviewsScrapper command line.",
+        epilog='To get your application Id look into the AppStore link to you app, for example http://itunes.apple.com/pl/app/autobuser-warszawa/id335042980?mt=8 - app Id is the number between "id" and "?mt=0"',
+    )
+    parser.add_argument(
+        "-i",
+        "--id",
+        default=0,
+        metavar="AppId",
+        type=int,
+        help="Application Id (see below)",
+    )
+    parser.add_argument(
+        "-c",
+        "--country",
+        metavar='"Name"',
+        type=str,
+        default="all",
+        help="AppStore country name (use -l to see them)",
+    )
+    parser.add_argument(
+        "-l", "--list", action="store_true", default=False, help="AppStores list"
+    )
+    parser.add_argument(
+        "-m",
+        "--max-reviews",
+        default=-1,
+        metavar="MaxReviews",
+        type=int,
+        help="Max number of reviews to load",
+    )
+    parser.add_argument(
+        "-r", "--raw-mode", action="store_true", default=False, help="Print raw mode"
+    )
     args = parser.parse_args()
     if args.id == 0:
         parser.print_help()
@@ -254,20 +291,23 @@ if __name__ == '__main__':
         for c in countries:
             print c
     else:
-        if (country == "All"):
+        if country == "All":
             rankCount = 0
             rankSum = 0
             for c in countries:
-                reviews = getReviews(
-                    appStores[c], args.id, maxReviews=args.max_reviews)
+                reviews = getReviews(appStores[c], args.id, maxReviews=args.max_reviews)
                 (rc, rs) = _print_reviews(reviews, c)
                 rankCount += rc
                 rankSum += rs
-            print "\nTotal number of reviews: %d, avg rank: %.2f" % (rankCount, 1.0 * rankSum / rankCount)
+            print "\nTotal number of reviews: %d, avg rank: %.2f" % (
+                rankCount,
+                1.0 * rankSum / rankCount,
+            )
         else:
             try:
                 reviews = getReviews(
-                    appStores[country], args.id, maxReviews=args.max_reviews)
+                    appStores[country], args.id, maxReviews=args.max_reviews
+                )
                 if args.raw_mode:
                     _print_rawmode(reviews)
                 else:
